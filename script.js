f33a107c5ea64dc0d8b26e33a01ccd12b8c2635e
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-10-05 22:30:11 +0530

    Updated Script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-Script
+import { ChatHandler, chat_names } from './ChatHandler.js';
+
+onload = function () {
+
+    const chatlist = document.getElementById('chat-list');
+    const add = document.getElementById('generate-step');
+    const text = document.getElementById('temptext');
+
+    const templates = document.getElementsByTagName('template')[0];
+    const chat_item = templates.content.querySelector("li");
+
+    const chatHandler = new ChatHandler(chat_item, chatlist);
+    let chats = [];
