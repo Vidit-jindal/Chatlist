112fb8cd35d1ed233326e8af4401028c57317357
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-10-06 23:42:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ onload = function () {
 
     const chatHandler = new ChatHandler(chat_item, chatlist);
     let chats = [];
+
+    add.onclick = function () {
+        if(Math.random()>0.75 && chats.length > 0){
+            let index = Math.floor(Math.random()*chats.length);
+            let idToDelete = chats[index];
+            chatHandler.deleteMsg(idToDelete);
+            text.innerHTML = "Deleted message from "+chat_names[idToDelete] + "<br>" + text.innerHTML;
+                chats.splice(index, 1);
+        } else{
+            let idOfMsg = Math.floor(Math.random()*7);
+            if(chats.includes(idOfMsg)===false){
+                chats.push(idOfMsg);
+            }
+            chatHandler.newMsg(idOfMsg);
+            text.innerHTML = "New message from "+chat_names[idOfMsg] + "<br>" + text.innerHTML;
+        }
+    };
+};
