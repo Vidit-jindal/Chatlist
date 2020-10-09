b210bacf9f52c4c4e6b82619a3462c6e565d18d4
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-10-09 23:25:37 +0530

    Update chatHandler.js

--- chatHandler.js
+++ chatHandler.js
@@ -1,1 +1,11 @@
 //This file will consist the javascript code
+export { ChatHandler, chat_names}
+
+const chat_names = ["Prateek Bhaiya", "Arnav Bhaiya", "Munna Bhaiya", "Aarnav Jindal", "Shriya Chhabra", "Akul Jindal", "Abhinav Duggal"];
+const chat_names_length = chat_names.length;
+const chat_msg = ["Why didn't he come and talk to me himse...",
+    "Perfect, I am really glad to hear that!...",
+    "This is what I understand you're telling me..",
+    "I’m sorry, I don’t have the info on that.."];
+const chat_msg_length = chat_msg.length;
+const chat_img_length = 7;
